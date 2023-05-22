175d2912e35915a06de9939c9248e567339b8208
Thiago Henrique Heinle Sanchez <54370834+ThiagoHs1@users.noreply.github.com>
2023-05-22 16:09:11 -0300

    Finalizando a tarefa

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
-  alert('Hello JQuery!');
+  // Manipula o evento de submissão do formulário
+  $('#taskForm').submit(function (event) {
+    event.preventDefault(); // Impede o recarregamento da página
+
+    // Obtém o valor do campo de entrada
+    var taskName = $('#taskInput').val();
+
+    // Cria um novo item de lista <li> com a tarefa
+    var listItem = $('<li>').text(taskName);
+
+    // Adiciona a classe completed ao clicar no item
+    listItem.click(function () {
+      $(this).toggleClass('completed');
+    });
+
+    // Adiciona o item de lista à lista de tarefas
+    $('#taskList').append(listItem);
+    // Limpa o campo de entrada
+    $('#taskInput').val('');
+  });
 });
