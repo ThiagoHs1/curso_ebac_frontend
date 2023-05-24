15490a0374e2379fa46553cec34eeb39213a0977
Istan <thiago.sh26@gmail.com>
2023-05-23 22:04:13 -0300

    terminando o exercicio

--- js/main.js
+++ js/main.js
@@ -2,4 +2,36 @@ $(document).ready(function () {
   $('#campo-telefone').mask('(00) 00000-0000');
   $('#campo-cep').mask('00000-000');
   $('#campo-cpf').mask('000.000.000-00');
+  $('form').validate({
+    rules: {
+      nome: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true,
+      },
+      numero: {
+        required: true,
+      },
+      cep: {
+        required: true,
+      },
+      CPF: {
+        required: true,
+      },
+    },
+    messages: {
+      nome: 'Insira seu Nome:',
+      email: 'Insira seu Email:',
+      numero: 'Insira seu Número:',
+      cep: 'Insira seu CEP:',
+      CPF: 'Insira seu CPF:',
+    },
+
+    errorPlacement: function (error, element) {
+      error.addClass('mensagem-erro');
+      error.insertAfter(element);
+    },
+  });
 });
