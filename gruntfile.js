4ff0c31ddae6ebb80a1a654c89af2e5da0cf03a3
Istan <thiago.sh26@gmail.com>
2023-06-17 17:25:49 -0300

    compiler less

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,25 @@
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        less: {
+            development: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            },
+            production: {
+                options : {
+                    compress: true,
+                },
+                files: {
+                    'main.min.css': 'main.less'
+                }
+            }
+        }
     })
 
-    grunt.registerTask('default', ['']);
+    grunt.loadNpmTasks('grunt-contrib-less');
+    
+
+    grunt.registerTask('default', ['less']);
 }
